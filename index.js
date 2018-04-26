8c0863550f1c899e3d9f375cdc03665714f08c32
jonschlinkert <jon.schlinkert@sellside.com>
2018-04-26 06:42:12 -0400

    export directly
    we don't need this to be a factory. going forward, implementors should create their own Token class if something different is needed.

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
 'use strict';
 
-const typeOf = require('kind-of');
+/**
+ * Create a new `Token` with the given `value` and `type`.
+ *
+ * ```js
+ * const token = new Token('*', 'Star');
+ * const token = new Token({type: 'star', value: '*'});
+ * console.log(token) //=> Token { type: 'star', value: '*' }
+ * ```
+ * @name Token
+ * @param {String|Object} `type` The token type to use when `value` is a string.
+ * @param {String} `value` Value to set
+ * @return {Object} Token instance
+ * @api public
+ */
 
-function create(options) {
-  const opts = Object.assign({value: 'value'}, options);
-
-  /**
-   * Create a new `Token` with the given `value` and `type`.
-   *
-   * ```js
-   * const token = new Token('*', 'Star');
-   * const token = new Token({type: 'star', value: '*'});
-   * console.log(token) //=> Token { type: 'star', value: '*' }
-   * ```
-   * @param {String|Object} `type` The token type to use when `value` is a string.
-   * @param {String} `value` Value to set
-   * @return {Object} Token instance
-   * @api public
-   */
-
-  class Token {
-    constructor(type, value, match) {
-      Reflect.defineProperty(this, 'isToken', {writable: true, value: true});
-
-      if (Array.isArray(value)) {
-        match = value;
-        value = type[opts.value] || match[0];
-      }
-
-      if (typeOf(type) === 'object') {
-        for (let key in type) this[key] = type[key];
-      } else {
-        this.type = type;
-        this[opts.value] = value;
-      }
-
-      if (match) {
-        this.match = match;
-      }
+class Token {
+  constructor(type, value, match) {
+    if (Array.isArray(value)) {
+      match = value;
+      value = type.value || match[0];
     }
 
-    static isToken(token) {
-      return token && token.isToken === true;
+    if (isObject(type)) {
+      for (const key in type) {
+        this[key] = type[key];
+      }
+    } else {
+      this.type = type;
+      this.value = value;
+    }
+
+    if (match) {
+      this.match = match;
     }
   }
 
-  /**
-   * Expose `Token` class
-   */
+  get isToken() {
+    return true;
+  }
 
-  return Token;
-};
+  static isToken(token) {
+    return isObject(token) && token.isToken === true;
+  }
+}
 
-module.exports = create();
-module.exports.create = create;
+function isObject(val) {
+  return val && typeof val === 'object' && !Array.isArray(val);
+}
+
+module.exports = Token;
